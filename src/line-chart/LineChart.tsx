5e968a8bb268732bc3b1e67f394e574b137b1db4
Zolt Health <zolthealth@gmail.com>
2024-10-20 12:35:47 -0400

    line chart should use all available space

--- src/line-chart/LineChart.tsx
+++ src/line-chart/LineChart.tsx
@@ -653,9 +653,10 @@ class LineChart extends AbstractChart<LineChartProps, LineChartState> {
   };
 
   getXMaxValues = (data: Dataset[]) => {
-    return data.reduce((acc, cur) => {
-      return cur.data == null ? acc : cur.data.length > acc ? cur.data.length : acc;
+    const xmax = data.reduce((acc, cur) => {
+      return cur.data.length > acc ? cur.data.length : acc;
     }, 0);
+    return Math.max(1, xmax - 1);
   };
 
   getBezierLinePoints = (
